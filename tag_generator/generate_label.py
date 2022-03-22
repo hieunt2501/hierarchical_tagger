c8d0c9935e62ae1383cee130930adc140dc41078
Hieu Nguyen <nthieu2501@gmail.com>
2022-03-22 22:41:34 +0700

    fix align char

--- tag_generator/generate_label.py
+++ tag_generator/generate_label.py
@@ -6,8 +6,7 @@ from utils.preprocess_data import perfect_align
 
 
 def process_labels(correct, incorrect):
-    _, labels = perfect_align(incorrect, correct, 2)
-    # print(labels)
+    _, labels = perfect_align(incorrect, correct, 3)
     final_label = []
     skip_flag = False
     for idx in range(len(labels)):
@@ -20,11 +19,17 @@ def process_labels(correct, incorrect):
                 if idx + 1 == len(labels) - 1:
                     break
             elif label[0].startswith("APPEND") and next_label[0].startswith("APPEND"):
-                tmp_label, tmp_e = final_label[-1]
-                tmp_label = tmp_label + next_label[0][-1]
-                final_label[-1] = [tmp_label, tmp_e]
-                if idx + 1 == len(labels) - 1:
-                    break
+                if final_label:
+                    tmp_label, tmp_e = final_label[-1]
+                    tmp_label = tmp_label + next_label[0][-1]
+                    final_label[-1] = [tmp_label, tmp_e]
+                    if idx + 1 == len(labels) - 1:
+                        break
+                else:
+                    tmp_label, tmp_e = label
+                    tmp_label = tmp_label + next_label[0][-1]
+                    final_label.append([tmp_label, tmp_e])
+                    skip_flag = True
             elif skip_flag:
                 skip_flag = False
             else:
@@ -32,20 +37,40 @@ def process_labels(correct, incorrect):
                 skip_flag = False
         else:
             final_label.append(label)
-    
-    assert len(final_label)==len(incorrect), f"Length labels not equal for sentence: {incorrect}"
-    # print(final_label)
+
+    if len(final_label) == len(incorrect) + 1:
+        if final_label[0][1] != final_label[1][1]:
+            print(f"Length labels not equal for sentence: {incorrect}")
+            raise Exception
+
+    elif len(final_label) != len(incorrect):
+        print(f"Length labels not equal for sentence: {incorrect}")
+        raise Exception
+
     return [label[0] for label in final_label]
 
 
-def save_file(corrects, incorrects, labels):
-    tmp_df = pd.DataFrame({"incorrect": incorrects, "correct": corrects, "label": labels})
-    with open("./labeled_dataset.pkl", "wb") as f:
-        pickle.dump(tmp_df, f)
+def extract_general_tag(label):
+    for i in range(len(label)):
+        if label[i].startswith("REPLACE") or label[i].startswith("APPEND"):
+            label[i] = label[i].split("_")[0]
+    return label
+
+
+def save_file(corrects, incorrects, edit_tags, general_tags, pkl_file=False):
+    tmp_df = pd.DataFrame({"incorrect": incorrects, 
+                            "correct": corrects, 
+                            "edit_tag": edit_tags,
+                            "general_tag": general_tags})
+    if pkl_file:
+        with open("./labeled_dataset.pkl", "wb") as f:
+            pickle.dump(tmp_df, f)
+    else:
+        tmp_df.to_csv("labeled_dataset.csv", encoding="utf8")
 
 
 def main():
-    df = pd.read_csv("./augment_data.csv", encoding="utf8")
+    df = pd.read_csv("./data/augment_data.csv", encoding="utf8")
     df.correct = df.correct.str.strip()
     df.incorrect = df.incorrect.str.strip()
 
@@ -53,11 +78,15 @@ def main():
     incorrects = df.incorrect.values.tolist()
 
     bar = tqdm(total=len(corrects), position=0, leave=True)
-    labels = []
+    edit_tags = []
+    general_tags = []
     for correct, incorrect in zip(corrects[:5], incorrects[:5]):
-        labels.append(process_labels(correct, incorrect))
+        edit_tag = process_labels(correct, incorrect)
+        general_tag = extract_general_tag(edit_tag.copy())
+        edit_tags.append(edit_tag)
+        general_tags.append(general_tag)
         bar.update(1)
-    save_file(corrects[:5], incorrects[:5], labels)
+    save_file(corrects[:5], incorrects[:5], edit_tags, general_tags)
 
 
 if __name__ == "__main__":
